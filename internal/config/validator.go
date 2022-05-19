5e8f8829b118c307fd58633043fd434a9b8e2a89
rageofgods <rageofgods@gmail.com>
2022-05-19 19:39:53 +0300

    refactor: validator.go optimise structure

--- internal/config/validator.go
+++ internal/config/validator.go
@@ -8,45 +8,49 @@ import (
 func (c *NexusConfig) ValidateConfig() error {
 	// Check server required parameters and setup defaults if they are missing
 	if c.Server != nil {
-		if c.Server.Port == "" {
+		switch {
+		case c.Server.Port == "":
 			c.Server.Port = serverPort
-		} else if c.Server.BindAddress == "" {
+		case c.Server.BindAddress == "":
 			c.Server.BindAddress = serverBindAddress
-		} else if len(c.Server.Credentials) == 0 {
+		case len(c.Server.Credentials) == 0:
 			return fmt.Errorf("error: server required 'credentials' variable is missing in %s", c.string)
-		} else if c.Server.Concurrency == 0 {
+		case c.Server.Concurrency == 0:
 			c.Server.Concurrency = clientConcurrency
-		} else if c.Server.TLS.Enabled { // Check for TLS parameters
-			if c.Server.TLS.Auto {
-				if c.Server.TLS.DomainName == "" {
-					return fmt.Errorf("error: server required 'domainName' variable is missing in %s", c.string)
-				}
-			} else {
-				if c.Server.TLS.KeyPath == "" || c.Server.TLS.CertPath == "" {
-					return fmt.Errorf("error: you must set 'KeyPath' and 'CertPath' variables in %s", c.string)
-				}
+		case c.Server.TLS.Enabled && c.Server.TLS.Auto:
+			if c.Server.TLS.DomainName == "" {
+				return fmt.Errorf("error: server required 'domainName' variable is missing in %s", c.string)
 			}
+		case c.Server.TLS.Enabled && !c.Server.TLS.Auto:
+			if c.Server.TLS.KeyPath == "" || c.Server.TLS.CertPath == "" {
+				return fmt.Errorf("error: you must set 'KeyPath' and 'CertPath' variables in %s", c.string)
+			}
+		default:
+			return nil
 		}
-		return nil
 	}
+
 	// Check client required parameters
 	if c.Client != nil {
-		if c.Client.SyncConfigs == nil {
-			return fmt.Errorf("error: client required 'syncConfigs' variable is missing in %s", c.string)
-		} else if c.Client.ServerAuth.User == "" {
+		switch {
+		case c.Client.ServerAuth.User == "":
 			return fmt.Errorf("error: client required 'serverAuth.user' variable is missing in %s", c.string)
-		} else if c.Client.ServerAuth.Pass == "" {
+		case c.Client.ServerAuth.Pass == "":
 			return fmt.Errorf("error: client required 'serverAuth.pass' variable is missing in %s", c.string)
-		} else if c.Client.Server == "" {
+		case c.Client.Server == "":
 			return fmt.Errorf("error: client required 'server' variable is missing in %s", c.string)
-		} else if c.Client.Daemon.SyncEveryMinutes == 0 {
+		case c.Client.Daemon.SyncEveryMinutes == 0:
 			c.Client.Daemon.SyncEveryMinutes = clientDaemonSyncEveryMinutes
-		}
-		// Check SynConfigs parameters
-		for _, v := range c.Client.SyncConfigs {
-			if v.Format == "" {
-				return fmt.Errorf("error: syncconfig required 'format' variable is missing in %s", v)
+		case c.Client.SyncConfigs == nil:
+			return fmt.Errorf("error: client required 'syncConfigs' variable is missing in %s", c.string)
+		case c.Client.SyncConfigs != nil:
+			for _, v := range c.Client.SyncConfigs {
+				if v.Format == "" {
+					return fmt.Errorf("error: syncconfig required 'format' variable is missing in %s", v)
+				}
 			}
+		default:
+			return nil
 		}
 	}
 
