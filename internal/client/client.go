67ca2167d9548d4de65fe2bdab212dc1ff2a06a6
rageofgods <rageofgods@gmail.com>
2022-06-06 18:50:33 +0300

    fix: compareComponents fixed bug with asset duplicated name

--- internal/client/client.go
+++ internal/client/client.go
@@ -38,7 +38,7 @@ func compareComponents(src []*comps.NexusComponent, dst []*comps.NexusComponent)
 	dstNca := make(map[string]struct{}, len(dst))
 	for _, v := range dst {
 		for _, vv := range v.Assets {
-			dstNca[fileNameFromPath(vv.Path)] = struct{}{}
+			dstNca[vv.Path] = struct{}{}
 		}
 	}
 
@@ -47,7 +47,7 @@ func compareComponents(src []*comps.NexusComponent, dst []*comps.NexusComponent)
 	for i, v := range src {
 		var nca []*comps.NexusComponentAsset
 		for ii, vv := range v.Assets {
-			if _, ok := dstNca[fileNameFromPath(vv.Path)]; !ok {
+			if _, ok := dstNca[vv.Path]; !ok {
 				nca = append(nca, v.Assets[ii])
 			}
 		}
